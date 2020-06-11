8303c56c90454d0f611f798e4ca88a08375b4ac7
kyle Dornan <kyledornan26@gmail.com>
2020-06-11 14:49:56 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,34 @@
+class Artist
+  @@count = 0
+  attr_accessor :name, :song
+  
+  def initialize(name)
+     @songs = []
+  @name = name 
+  end 
+  def songs 
+    @songs << songs.name 
+  end 
+  
+  def songs 
+    Song.all.select do |song|
+      song.artist == self 
+    end 
+  end 
+  def add_song(song)
+    @songs << song 
+    song.artist = self
+    @@count += 1 
+     end
+     
+     def add_song_by_name(name)
+       song = Song.new(name)
+       add_song(song)
+     end 
+
+     def self.song_count
+    @@count +=1 
+  end 
+           
+  
+    end 
---
+++ lib/author.rb
@@ -0,0 +1,23 @@
+class Author 
+  
+  attr_accessor :name, :posts
+  
+  
+  def initialize(name)
+    @name = name 
+    @posts = []
+  end 
+def posts 
+  @posts << posts.name 
+  
+end 
+   def posts
+     Posts.all.select do  |posts| 
+     posts.author == self 
+ end    
+ 
+end
+ 
+   
+  
+end 
