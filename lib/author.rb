ef1b90e95656e728c439c28b564c15b4b562a7b1
kyle Dornan <kyledornan26@gmail.com>
2020-06-11 14:55:00 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -7,17 +7,7 @@ class Author
     @name = name 
     @posts = []
   end 
-def posts 
-  @posts << posts
-  
-end 
-   def posts
-     Posts.all.select do  |posts| 
-     posts.author == self 
- end    
- 
-end
- 
+
    
   
 end 
