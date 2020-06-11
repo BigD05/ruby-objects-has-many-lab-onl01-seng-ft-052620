f3f1e16bc40e0a02943e4869a64c411504e4afdc
kyle Dornan <kyledornan26@gmail.com>
2020-06-11 15:08:23 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,33 @@
+class Author 
+  
+  attr_accessor :name, :posts
+  @@count = 1 
+  
+  def initialize(name)
+    @name = name 
+    @posts = []
+  end 
+
+   def posts 
+    @posts <<  posts.name  
+   end 
+   def posts 
+  Post.all.select do |name| 
+    name.author == self 
+  end 
+end 
+
+def add_post(posts) 
+  @posts << posts 
+  posts.author = self 
+  @@count += 1 
+  end 
+  def add_post_by_title(title)
+    posts = Post.new(title) 
+    add_post(posts)
+    end 
+    def self.post_count 
+      @@count
+    end 
+  
+end 
